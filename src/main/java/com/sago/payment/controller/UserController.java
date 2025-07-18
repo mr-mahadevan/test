2565921b6849fd985f19c727aae76c82c8f68c69
= <=>
2025-07-18 22:19:30 +0530

    new project

--- src/main/java/com/sago/payment/controller/UserController.java
+++ src/main/java/com/sago/payment/controller/UserController.java
@@ -1,16 +1,29 @@
 package com.sago.payment.controller;
 
 import com.sago.payment.dto.UserRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @RestController
-@RequestMapping(value = "api/users")
+@RequestMapping(value = "api/users", produces = "application/json", consumes = "application/json")
 public class UserController {
 
-    @PostMapping
-    public ResponseEntity<UserRequest> saveUser(@RequestBody @Valid UserRequest userRequest){
+
+    @PostMapping(
+            name = "Save User",
+            value = "/save",
+            produces = "application/json",
+            consumes = "application/json",
+            headers = "Content-Type=application/json")
+    public ResponseEntity<UserRequest> saveUser(@RequestBody @Valid UserRequest userRequest, HttpServletRequest request){
+        log.info("User IP: {}" , request.getRemoteAddr());
         return ResponseEntity.ok(userRequest);
     }
 }
